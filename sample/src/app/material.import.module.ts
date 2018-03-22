ff27fe23bad5fe90cb079bd3aec328a6f2d16291
Aslam Sayyed <aslam.sayyed@gmail.com>
2018-03-22 17:08:59 +0530

    Flex layout integration

--- sample/src/app/material.import.module.ts
+++ sample/src/app/material.import.module.ts
@@ -6,15 +6,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-
+import { FlexLayoutModule } from '@angular/flex-layout'
 @NgModule({
   imports: [
+    FlexLayoutModule,
     BrowserAnimationsModule,
     MatSnackBarModule,
     MatButtonModule,
     MatIconModule
   ],
   exports: [
+    FlexLayoutModule,
     BrowserAnimationsModule,
     MatSnackBarModule,
     MatButtonModule,

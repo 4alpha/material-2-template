c88e6dedc77227e48f24e1efc8e2ffbe08dc531b
Aslam Sayyed <aslam.sayyed@gmail.com>
2018-03-22 14:35:27 +0530

    Setup material 2

---
+++ sample/src/app/material.import.module.ts
@@ -0,0 +1,24 @@
+import 'web-animations-js';
+import 'hammerjs';
+
+import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
+@NgModule({
+    imports: [
+        BrowserAnimationsModule,
+        MatSnackBarModule,
+        MatButtonModule,
+        MatIconModule
+    ],
+    exports: [
+        BrowserAnimationsModule,
+        MatSnackBarModule,
+        MatButtonModule,
+        MatIconModule
+    ]
+  })
+  export class MaterialImportModule { }
